2f8d669b2eaf2c44acc6011235cc65a1efeef590
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-08-09 16:54:08 -0300

    Created the template and exercises for the group

---
+++ day_1/day_1.py
@@ -0,0 +1,45 @@
+# Variables and Math Operations
+
+# Declare tw variables that store numbers and print() a sum, a subtraction and division and a multiplication
+
+a = 20
+b = 5
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+
+# Data Types
+
+number = 10000  # Int
+sentense = "I want to type a sentense"  # String
+floatNumber = 10.4  # Float
+
+first_sentence = "All you need is "
+second_sentence = "a cheeseburger"
+third_sentence = ", love"
+fourth_sentence = " and a lot of money"
+number_as_string = '10000'
+# str() - turns a data type into a String data type
+# int() - turns a data type into a Int Number data type
+
+print(first_sentence + str(number))
+print(int(number_as_string) * 2)
+
+# List Data Type
+
+list_names = ["Hugo", "Leticia", "Vini", "Andrew", "Imtiaj"]
+
+print(list_names[0])
+
+# Lists are group of data stored together by the use of [ ]
+# We can access the items on the list by using the index selector list_name[0]
+# Always remember that Python is 0 indexed, therefore we start counting on 0, not 1
+
+list_from_one_to_ten = list(range(1, 11))
+
+print(list_from_one_to_ten)
+
+# list() is used to create a list type of data
+# range(0, 11) creates a list of numbers starting at 1 and ending, but not including 11
---
+++ day_1/day_1_template.py
@@ -0,0 +1,22 @@
+# Variables and Math Operations
+
+# Declare tw variables that store numbers and print() a sum, a subtraction and division and a multiplication
+# Your code below this line
+
+
+# Data Types
+# Create a String
+# Create an Int
+# Create a Flaot
+# Print two strings being concatenated
+# Print two numbers being multiplied
+# Print a string being multiplied
+# Your code goes below this line
+
+
+# List Data Type
+# Make a list with 5 names
+# print the second and the fifth name of your list
+# Make a list with a range from 1 to 20
+# Print the range list
+# Your code goes below this line
