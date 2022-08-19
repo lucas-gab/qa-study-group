d9e9e1d8a028faba11cb7e166a04d064ed91972e
cameronclegg <cameron.clegg@carta.com>
2022-08-19 15:01:52 -0600

    heh, I think it worked now?

--- day_1/day_1_cameron.py
+++ day_1/day_1_cameron.py
@@ -3,26 +3,43 @@
 # Declare tw variables that store numbers and print() a sum, a subtraction and division and a multiplication
 # Your code below this line
 
+a = 100
+b = 10
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
 
 # Data Types
 # Create a String
+message_1 = "I'm "
+message_2 = "rolling with the homies"
 # Create an Int
+myInt = 5
 # Create a Flaot
+myFloat = 5.5
 # Print two strings being concatenated
+print(message_1 + message_2)
 # Print two numbers being multiplied
+print(myInt * myFloat)
 # Print a string being multiplied
-# Your code goes below this line
-
+print(message_2 * myInt)
 
 # List Data Type
 # Make a list with 5 names
+myList = ["Lucas", "Cameron", "Let", "Andrew", "Edu"]
 # print the second and the fifth name of your list
-# Make a list with a range from 1 to 20
-# Print the range list
-# Your code goes below this line
+print(myList[1])
+print(myList[4])
 
+# Make a list with a range from 1 to 20
+myRange = [*range(1, 20)]
+# Print the range list
+print(myRange)
 
 # Declare a variable my_name and assign a string to it with your name
+myName = "Cameron"
 # Write your name with funky captalization like lUcAs
 # print my_name on lowercase only (use .lower())
 # print my_name on uppercase only (use .upper())
