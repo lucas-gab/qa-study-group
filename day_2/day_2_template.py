3717619d80c7a1fb9874abb420af99b9fb08d515
AlexKorg <aleks.korzhan@gmail.com>
2022-08-30 22:53:35 -0600

    Please check for mistakes

--- day_2/day_2_template.py
+++ day_2/day_2_template.py
@@ -23,9 +23,26 @@ student_3_fourth_grade = 4
 # If the student has a grade greater than or equal 8, then they are free to enjoy their summer vacations
 # If the student has a grade smaller than 8, but higher or equal to 6, then they are getting some summer homework
 # If the student has a grade smaller than 6, then they will stay in school during summer
-
 # Declare a function that calculates the final grade of each student
-# The same function will have to print a message to the terminal stating if the student will have summer vacations, 
+# The same function will have to print a message to the terminal stating if the student will have summer vacations,
 # will have summer homework or will have to stay in school during summer
 # Use functions and conditionals to achieve this
 # Your code goes below this line
+
+
+def calculate_final_grade(student_name, first_grade, second_grade, third_grade, fourth_grade):
+    final_grade = (first_grade + second_grade + third_grade + fourth_grade) / 4
+
+    print(f"{student_name}'s final grade is {final_grade}")
+
+    if final_grade >= 8:
+        print("Free to enjoy summer vacation.")
+    elif final_grade < 8 and final_grade >= 6:
+        print("Getting some summer homework.")
+    else:
+        print('Stays in school during summer.')
+
+
+calculate_final_grade("Maria", 10, 8, 7, 10)
+calculate_final_grade("Joseph", 6, 4, 2, 6)
+calculate_final_grade("Helen", 10, 9, 5, 4)
