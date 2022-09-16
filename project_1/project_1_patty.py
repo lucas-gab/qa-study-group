e7e1f65a5124c15f9786a05ae396a30e7783d574
pattycsouza <patricia.souza@carta.com>
2022-09-16 17:59:20 -0300

    lesson 8 project completed

--- project_1/project_1_patty.py
+++ project_1/project_1_patty.py
@@ -1,13 +1,13 @@
 ######Fundamentals
-######Say something: (input)  until  \end to end the program
+######Say something: (input)  until  /end to end the program
 
 
-# We will need a while loop with an "input", that continue runing until the input = "\end".
+# We will need a while loop with an "input", that continue runing until the input = "/end".
 # It will print all the phrases, with the first letter capitalized and necessary punctuation.
 # Depending on the first word it will define a different punctuation.
 
 #function to convert the phrase into the formated one:
-def sentence_maker (phrase):
+def sentence_maker(phrase):
     interrogatives = ('how', 'what', 'when', 'where', 'why', 'who')     #words that defines questions: how, what, when, where, why
     capitalize = phrase.capitalize()
 
@@ -22,7 +22,7 @@ phrase_bank = []
 #Getting the info from the user:
 while True:
     user_phrase = input("Say something: ")
-    if user_phrase == "\end":
+    if user_phrase == "/end":
         break
     else:
         phrase_bank.append(sentence_maker(user_phrase))
